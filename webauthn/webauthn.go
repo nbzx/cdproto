75cf75f2847a1183347069a198743ec69ead252d
PaulXu <nbzx@live.cn>
2019-06-06 10:02:47 +0800

    更改包名

--- webauthn/webauthn.go
+++ webauthn/webauthn.go
@@ -12,7 +12,7 @@ package webauthn
 import (
 	"context"
 
-	"github.com/chromedp/cdproto/cdp"
+	"github.com/nbzx/cdproto/cdp"
 )
 
 // EnableParams enable the WebAuthn domain and start intercepting credential
